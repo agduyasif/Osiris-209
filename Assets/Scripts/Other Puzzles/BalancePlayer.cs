3241c0578aa6be78ad2a77ac1a64d6bcbd849360
Amr4k <catafranchi@gmail.com>
2026-07-16 11:08:56 -0300

    BUG puzzle
    solucione el bug del puzzle del equilibrio

--- Assets/Scripts/Other Puzzles/BalancePlayer.cs
+++ Assets/Scripts/Other Puzzles/BalancePlayer.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 
 public class BalancePlayer : MonoBehaviour
 {
-
     [Header("Referencias")]
     [SerializeField] private InputActionReference movecontrol;
     [SerializeField] private Camera playerCamera;
-    [SerializeField] private float speed = 5f; 
+    [SerializeField] private float speed = 5f;
+
+    [Header("Script de Movimiento Normal")]
+    [SerializeField] private MonoBehaviour scriptMovimientoNormal;
 
     private Rigidbody rb;
     private BalanceLogic balanceLogic;
 
     public bool IsBalancing { get; private set; }
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
         balanceLogic = new BalanceLogic();
@@ -30,29 +32,48 @@ public class BalancePlayer : MonoBehaviour
         Vector3 dirAdelante = transform.forward * input.y;
         balanceLogic.TubeMove(dirAdelante, speed, rb);
 
-        float inclinacion = balanceLogic.balanceHandle * 20f;
-        playerCamera.transform.localRotation = Quaternion.Euler(0, 0, -inclinacion);
-
         if (balanceLogic.CheckIfFallen())
         {
-            IsBalancing = false;
+            TerminarEquilibrio();
             ResetScene.Reset();
         }
+    }
+
+    void LateUpdate()
+    {
+        if (!IsBalancing) return;
+        float inclinacion = balanceLogic.balanceHandle * 20f;
+        playerCamera.transform.localRotation = Quaternion.Euler(0, 0, -inclinacion);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Equilibrio"))
         {
+            Debug.Log("Toqué el inicio del cilindro");
             IsBalancing = true;
             balanceLogic.Reset();
+
+            if (scriptMovimientoNormal != null)
+            {
+                scriptMovimientoNormal.enabled = false;
+            }
         }
 
         if (other.CompareTag("Fin"))
         {
-            IsBalancing = false;
-            playerCamera.transform.localRotation = Quaternion.identity;
+            TerminarEquilibrio(); 
         }
     }
 
+    public void TerminarEquilibrio()
+    {
+        IsBalancing = false;
+        playerCamera.transform.localRotation = Quaternion.identity;
+
+        if (scriptMovimientoNormal != null)
+        {
+            scriptMovimientoNormal.enabled = true;
+        }
+    }
 }
